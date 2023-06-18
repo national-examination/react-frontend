f4acc05785e0e42f4652fc0aed54da1d1e0d90db
Esther10203 <eumuhoza83@gmail.com>
2023-06-19 01:32:05 +0200

    First commit

--- src/Screens/Settings.jsx
+++ src/Screens/Settings.jsx
@@ -8,6 +8,7 @@ import Account from '../components/Account';
 import DataTable from 'react-data-table-component';
 
 function Settings() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { isLoggedIn } = useContext(AuthContext);
   const navigate = useNavigate();
 
@@ -28,7 +29,7 @@ function Settings() {
       sortable: true
     },
   ];
-  
+
   const data = [
     {
       id: 1,
@@ -44,9 +45,6 @@ function Settings() {
 
   const [records, setRecords] = useState(data);
 
-  // const [records, setRecords] = useState(data);
-  
-
   const handleFilter = (e) => {
     const newData = data.filter(row => {
       return row.title.toLowerCase().includes(e.target.value.toLowerCase());
@@ -54,6 +52,14 @@ function Settings() {
 
     setRecords(newData);
   }
+
+  const handleAddButtonClick = () => {
+    setIsModalOpen(!isModalOpen);
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
 
   return (
     <div className="flex">
@@ -65,8 +71,59 @@ function Settings() {
             <div>
               <h1 className="py-3 text-red-400 font-medium">All Settings</h1>
               <div className='my-3 flex flew-row justify-between'>
-                <input type="text" onChange={handleFilter} className="border border-gray-100 p-2" placeholder='Search ...'/>
-                <button className="bg-red-400 py-2 px-4 text-white ">+ Add</button>
+                <input type="text" onChange={handleFilter} className="border border-gray-100 p-2" placeholder='Search ...' />
+                <button onClick={handleAddButtonClick} className="bg-red-400 py-2 px-4 text-white ">+ Add</button>
+                {isModalOpen ? (
+                  <>
+                    <div className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none">
+                      <div className="relative w-auto my-6 mx-auto max-w-2xl px-6">
+                        {/*content*/}
+                        <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
+                          {/*header*/}
+                          <div className="p-5 border-b border-solid border-slate-200 rounded-t">
+                            <h3 className="text-lg font-medium text-center text-red-400">
+                              Add New Device
+                            </h3>
+                          </div>
+
+                          <form onSubmit={handleAddButtonClick}>
+                            {/*body*/}
+                            <div className="relative px-16 py-6 flex-auto">
+                              <div className="mb-4 flex items-center">
+                                <label className="w-36">Device Name</label>
+                                <input
+                                  className="border w-52 p-2"
+                                  type="text"
+                                  // onChange={(e) => setDeviceName(e.target.value)}
+                                  required
+                                  placeholder="Enter device name"
+                                />
+                              </div>
+                            </div>
+
+                            {/*footer*/}
+                            <div className="flex items-center justify-between px-12 py-3 border-t border-solid border-slate-200 rounded-b">
+                              <button
+                                className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                type="button"
+                                onClick={handleCloseModal}
+                              >
+                                Close
+                              </button>
+                              <button
+                                className={`bg-red-400 text-white active:bg-red-600 font-medium uppercase text-sm px-3 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
+                                type="submit"
+                              >
+                                Save Changes
+                              </button>
+                            </div>
+                          </form>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="opacity-25 fixed inset-0 z-40 bg-black"></div>
+                  </>
+                ) : null}
               </div>
               <DataTable columns={columns} data={records} selectableRows fixedHeader pagination />
             </div>
